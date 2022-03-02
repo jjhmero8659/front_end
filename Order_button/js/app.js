d9c9d0e8038a8a09940c67be23ccc5a799430631
jjhmero8659 <77915673+jjhmero8659@users.noreply.github.com>
2022-03-02 23:57:42 +0900

    Add files via upload

--- Order_button/js/app.js
+++ Order_button/js/app.js
@@ -1,14 +1,15 @@
 $(function(){
     var Prevent_overlap_pro_li = new Array();
+    
     const product_array = [
         ["Horizon zero dawn Complete","Horizon zero dawn Forbidden West"],
         ["MonsterHunter Normal","MonsterHunter Iceborn","MonsterHunter Rise"]
     ]
-    const price_array = [
+    const product_count_array = [
         ["0","0"],
         ["0","0","0"]
     ]
-    const product_count = [
+    const product_cost_array = [
         ["35000","47000"],
         ["22000","35000","53000"]
     ]
@@ -66,8 +67,7 @@ $(function(){
     function sec1_value_delivery(){
 
         if($(".section1 .gnb>li>a").text() == "Horizon zero dawn"){ //첫번째 section 의 text가 horizon이라면?
-            row = 0;
-            console.log(img_src_arr[row])
+            row = 0; 
             $(".img_wrap img").attr({"src":img_src_arr[row]});
 
             create_Dom();
@@ -86,9 +86,9 @@ $(function(){
             for(var i=0; i<product_array[row].length; i++){
                 var doc_a_text = document.createTextNode(product_array[row][i]);
                 
-                var doc_a = document.createElement("a");
+                var doc_a = document.createElement("a"); 
                 doc_a.setAttribute("href","javascript:void(0)"); //Dom a는 href = "#" 이 불안정?? , 대체
-                doc_a.onclick = function(){
+                doc_a.onclick = function(){ 
                     var tmp = this.textContent; //스왑 하기위한 임시 변수
 
                     if($(".section2 .gnb>li>a").text() == "Edition"){ //"Edition 을 hide"
@@ -119,21 +119,22 @@ $(function(){
             }
     }
 
-    function total_product_price(){
+    function total_product_price(){ //처음 리스트 추가시에 기본 가격 등 init
         for(var i=0; i<product_array[row].length; i++){
             if($(".section2 .gnb>li>a").text() == product_array[row][i]){
                 col = i;
-                total_price += Number(price_array[row][col]);
+                // console.log(Number(product_count_array[row][col]))
+                // total_price += Number(product_count_array[row][col]);
             }
         }
-        $(".Total_price_text").empty().append(total_price + "원"); //가격계산
+        $(".Total_price_text").empty().append("0원"); //가격계산
     }
 
     function add_product_list(e){
         var local_row = row;
         var local_col = col;
         pro_id_count++;
-        
+
         for(var i=0; i<Prevent_overlap_pro_li.length; i++){ // 중복방지
             if(product_array[local_row][local_col] == Prevent_overlap_pro_li[i]){
                 return 0;
@@ -141,7 +142,7 @@ $(function(){
         }
 
         var doc_pro = document.createElement("div");
-        doc_pro.setAttribute("id","num"+product_count);
+        // doc_pro.setAttribute("id","num"+pro_id_count);
         doc_pro.className = "product";
 
         var doc_span_text = document.createTextNode(product_array[local_row][local_col]);
@@ -149,37 +150,59 @@ $(function(){
         doc_span.appendChild(doc_span_text);
         Prevent_overlap_pro_li.push(product_array[local_row][local_col]); // 중복방지
 
-        var doc_count_text = document.createTextNode(price_array[local_row][local_col]);
+        var doc_count_text = document.createTextNode(product_count_array[local_row][local_col]);
         var doc_count_div = document.createElement("div");
         doc_count_div.className = "count";
         doc_count_div.appendChild(doc_count_text);
+        
+
+        var doc_delete_text = document.createTextNode("x"); // + 버튼
+        var doc_delete_a = document.createElement("a");
+        doc_delete_a.setAttribute("href","javascript:void(0)");
+        doc_delete_a.onclick = function(){
+            doc_pro.remove();
+            for(var i=0; i<Prevent_overlap_pro_li.length; i++){ // 제품선택시에 리스트 하나 추가후 delete 작동후 추가하면 error 발생
+                if(product_array[local_row][local_col] == Prevent_overlap_pro_li[i]){
+                    total_price -= (product_cost_array[local_row][local_col] * product_count_array[local_row][local_col]);
+                    product_count_array[local_row][local_col] = 0;
+                    doc_count_text.textContent = product_count_array[local_row][local_col];
+                    $(".Total_price_text").empty().append(total_price + "원");
+                    Prevent_overlap_pro_li.pop(Prevent_overlap_pro_li[i]); // 해당하는 문자열 중복에서 제거
+                }
+            }
+   
+        }
+        doc_delete_a.appendChild(doc_delete_text); 
+        var doc_delete_div = document.createElement("div");
+        doc_delete_div.className = "delete";
+        doc_delete_div.appendChild(doc_delete_a);
 
 
-        var doc_add_text = document.createTextNode("+");
+        var doc_add_text = document.createTextNode("+"); // + 버튼
         var doc_add_a = document.createElement("a");
         doc_add_a.setAttribute("href","javascript:void(0)");
         doc_add_a.onclick = function(){
-            total_price =  total_price + Number(product_count[local_row][local_col]);
-            price_array[local_row][local_col]++;
-            doc_count_text.textContent = price_array[local_row][local_col];
+            total_price =  total_price + Number(product_cost_array[local_row][local_col]);
+            product_count_array[local_row][local_col]++;
+            doc_count_text.textContent = product_count_array[local_row][local_col];
             $(".Total_price_text").empty().append(total_price + "원");
         }
-        doc_add_a.appendChild(doc_add_text);
+        doc_add_a.appendChild(doc_add_text); 
         var doc_add_div = document.createElement("div");
         doc_add_div.className = "add";
         doc_add_div.appendChild(doc_add_a);
 
 
 
-        var doc_remove_text = document.createTextNode("-");
+        var doc_remove_text = document.createTextNode("-"); // - 버튼
         var doc_remove_a = document.createElement("a");
         doc_remove_a.setAttribute("href","javascript:void(0)");
         doc_remove_a.className = "remove";
         doc_remove_a.onclick = function(){
-            if(price_array[local_row][local_col] > 0){
-                total_price = total_price - Number(product_count[local_row][local_col]);
-                price_array[local_row][local_col]--;
-                doc_count_text.textContent = price_array[local_row][local_col];
+            if(product_count_array[local_row][local_col] > 0){
+                total_price = total_price - Number(product_cost_array[local_row][local_col]);
+                product_count_array[local_row][local_col]--;
+                doc_count_text.textContent = product_count_array[local_row][local_col];
                 $(".Total_price_text").empty().append(total_price + "원");
             } 
         }
@@ -192,6 +215,7 @@ $(function(){
         doc_pro.appendChild(doc_count_div);
         doc_pro.appendChild(doc_add_div);
         doc_pro.appendChild(doc_remove_div);
+        doc_pro.appendChild(doc_delete_div);
 
         document.getElementById("put_list_li").appendChild(doc_pro);
     }
