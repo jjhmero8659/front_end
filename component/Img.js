a1403def1255be70ee87a0f10a5fbaa0a118f989
jjhmero8659 <77915673+jjhmero8659@users.noreply.github.com>
2022-03-22 23:56:41 +0900

    Add files via upload

--- component/Img.js
+++ component/Img.js
@@ -1,19 +1,49 @@
 import React , {Component} from "react";
 import "./css/Img.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import jquery from 'jquery';
+import $ from 'jquery';
+
 
 class Img extends Component{
     constructor(props){
         super(props)
-
-        this.state = {
-
-        }
     }
-
+    
     render(){
+        var settings = {
+            dots: false,
+            autoplay:true,
+            infinite: true,
+            speed: 1000,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+
         return(
             <div id="Img_wrap">
-                <img src={require("./img/Elden_Ring.jpeg")}></img>
+                <Slider {...settings}>
+                    <div>
+                        <img src={require("./img/Elden_Ring.jpeg")}></img>
+                    </div>
+                    <div>
+                        <img src={require("./img/Elden_Ring1.jpg")}></img>
+                    </div>
+                        <div>
+                    <img src={require("./img/Elden_Ring2.jpg")}></img>
+                        </div>
+                    <div>
+                        <img src={require("./img/Elden_Ring3.jpg")}></img>
+                    </div>
+                        <div>
+                    <img src={require("./img/Elden_Ring4.jpg")}></img>
+                    </div>
+                        <div>
+                    <img src={require("./img/Elden_Ring5.jpg")}></img>
+                    </div>
+                </Slider>
             </div>
         )
     }
--- component/gpa.js
+++ component/gpa.js
@@ -9,7 +9,24 @@ class Gpa extends Component{
     render(){
         return(
             <div id="Gpa_wrap">
-                <img src={require("./img/gpa.png")}></img>
+                <div id="normal" className="Purchase">
+                    <span>ELDEN RING 구매</span>
+                    <div className="add_basket_wrap">
+                        ₩ 64,800
+                        <div className="add_basket">
+                            <a>장바구니에 추가</a>
+                        </div>
+                    </div>
+                </div>
+                <div id="delux" className="Purchase">
+                    <span>ELDEN RING Deluxe Edition 구매</span>
+                    <div className="add_basket_wrap">
+                        ₩ 86,800
+                        <div className="add_basket">
+                            <a>장바구니에 추가</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
