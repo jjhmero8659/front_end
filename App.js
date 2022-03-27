d3ac2067eb5b90c6202e73418bad871c353cb117
jjhmero8659 <77915673+jjhmero8659@users.noreply.github.com>
2022-03-27 17:31:18 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,74 +1,74 @@
-import React ,{Component} from "react";
+import React , {Component} from "react";
+import axios from "axios";
 import "./App.css";
 
-import List from "./Components/List.js"
-import Input from "./Components/Input.js"
+import Student from "./Component/Student.js"
+import InputComp from "./Component/InputComp.js"
 
 class App extends Component{
-  
   constructor(props){
     super(props);
 
     this.state = { 
-      Todo_List : [
-        {num:1,today:"코딩"},
-      ],
-      next_id : 2
+      studentList : [
+        {id:"2018038018",major:"컴퓨터 공학과",name:"정채연"},
+        {id:"2017038018",major:"시각 디자인과",name:"아린"},
+        {id:"2020183515",major:"전자 공학과",name:"아이유"},
+      ]
     }
   }
-  add_list = (input_today) =>{
-    var List_Obj = {
-      num : this.state.next_id,
-      today : input_today
+
+  addStudentInfo = (input_id,input_major,input_name) =>{
+    var Student_Obj = {
+      id : input_id,
+      major : input_major,
+      name : input_name,
     }
-    this.setState({
-      Todo_List : this.state.Todo_List.concat(List_Obj),
-      next_id:this.state.next_id+1
+    this.setState({ // 상태 값에 추가
+      studentList:this.state.studentList.concat(Student_Obj) //push는 값 반환 x concat 는 반환
     })
-
-    // this.setState({
-    //   next_id:this.state.next_id+1
-    // })
-
+    
   }
 
-  Del_list = (num) =>{
-    
-    const filter_List = this.state.Todo_List.filter(
-      (data) => (data.num != num)
+  Del_StudentInfo = (del_id)=>{
+    const filteredList = this.state.studentList.filter(
+      (data) => (data.id !== del_id)
     )
-    for(var i=0; i<filter_List.length; i++){
-      if(filter_List[i].num > num){
-        filter_List[i].num--;
-      }
-    }
-    
-    this.setState({
-      Todo_List : filter_List,
-      next_id:this.state.next_id-1
+    this.setState({ //통과된 데이터로 새로운 배열 생성
+      studentList : filteredList
     })
   }
 
+  uptStudentInfo = (id,major,name) => {
+    alert("수정함수(APP)")
+    alert("App에 넘어온 학번"+id)
+    alert("App에 넘어온 전공"+major)
+    alert("App에 넘어온 전공"+name)
+
+    const updateStudentObj = {id:id,major:major,name:name};
+    const updateStudentList = this.state.studentList.map(//3점연산자 (three dot operators)
+      (data) => (data.id == id) ? ({...data,...updateStudentObj}) : (data)
+    )
+    this.setState({
+      studentList : updateStudentList
+    })
+    
+  }
 
   render(){
-    const result = this.state.Todo_List.map(
-      (data,index) => (<List
+    const result = this.state.studentList.map(
+        (data,index) => <Student
           key = {index}
-          num = {data.num}
-          today = {data.today}
-          Del_list = {this.Del_list}
-      />)
-    )
-    const result_input =  
-      <Input
-        add_list = {this.add_list}
-    />
-    
-
+          id = {data.id}
+          major = {data.major}
+          name = {data.name}
+          Del_StudentInfo = {this.Del_StudentInfo}
+          uptStudentInfo = {this.uptStudentInfo}
+        />
+      )
     return(
       <div id="App_Class_wrap">
-        <header>Todo_List</header>
-        {result_input}
+        <InputComp addStudentInfo={this.addStudentInfo}/>
         {result}
       </div>
     )
--- Component/Student.js
+++ Component/Student.js
@@ -7,7 +7,10 @@ class Student extends Component{
     super(props);
 
     this.state = {
-
+      edit:false,
+      id:this.props.id,
+      major:this.props.major,
+      name:this.props.major,
     }
   }
 
@@ -16,24 +19,76 @@ class Student extends Component{
       this.props.Del_StudentInfo(this.props.id)
   }
 
+  uptStudentInfo = () =>{
+    alert("수정")
+      
+    //수정버튼을 눌러서, 수정 가능 모드 상태가 되고
+    //그때 input 창에 입력해서 ,입력된값을 읽어 app.js로 보냄 , 최종 수정
+    //수정 불가능 모드일때 수정 버튼을 누르면 ... 그냥 화면만 변경
+    if(this.state.edit ==true){
+      //edit 이 true 면 수정 가능 모드
+      alert("수정할 학번: " + this.props.id)
+      alert("수정될 전공: " + this.state.major)
+      alert("수정할 이름: " + this.props.name)
+      this.props.uptStudentInfo(this.props.id,this.state.major,this.state.name)
+      //App.js의 수정함수
+    }
+
+    this.setState({ //
+      edit : !this.state.edit
+  },function(){
+    console.log("callback in:"+ this.state.edit)
+  }) 
+  console.log(this.state.edit)//false
+    
+}
+
+majorChange = (e) =>{
+  this.setState({
+    [e.target.name]:e.target.value
+  })
+}
+
   render(){
-    return(
-      <div id="Student_Class_wrap">
-          <div>
-              학번 : {this.props.id}
-          </div>
-          <div>
-              전공 : {this.props.major}
-          </div>
-          <div>
-              이름 : {this.props.name}
-          </div>
-          <div>
-              <button onClick={this.Del_StudentInfo}>삭제</button>
-              <button onClick={this.Update_StudentInfo}>수정</button>
-          </div>
-      </div>
-    )
+    if(this.state.edit === false){
+      return(
+        <div id="Student_Class_wrap">
+            <div>
+                학번 : {this.props.id}
+            </div>
+            <div>
+                전공 : {this.props.major}
+            </div>
+            <div>
+                이름 : {this.props.name}
+            </div>
+            <div>
+                <button onClick={this.Del_StudentInfo}>삭제</button>
+                <button onClick={this.uptStudentInfo}>수정</button>
+            </div>
+        </div>
+      )
+    }
+    else{
+      return(
+        <div id="Student_Class_wrap">
+            <div>
+                학번 : {this.props.id}
+            </div>
+            <div>
+                전공 : <input type="text" name="major" onChange={this.majorChange} defaultValue={this.props.major}></input>
+            </div>
+            <div>
+                이름 : {this.props.name}
+            </div>
+            <div>
+                <button onClick={this.Del_StudentInfo}>삭제</button>
+                <button onClick={this.uptStudentInfo}>수정</button>
+            </div>
+        </div>
+      )
+    }
+    
   }
 }
 
